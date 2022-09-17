54b6e93121375d298b7e2573677793cd8593526e
Illiya91 <fortune.illiya@gmail.com>
2022-09-18 02:36:03 +0300

    My home work

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rozetka.com.ua");
+        Thread.sleep(10000);
+        driver.close();
+    }
+    }
